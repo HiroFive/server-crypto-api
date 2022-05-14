6f906108db93c9c2dc6d44acc7461ce6da5760ba
HiroFive <vanohahj5555@gmail.com>
2022-05-14 23:20:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors')
+require("dotenv").config();
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+const userRoutes = require("./routes/userRoutes");
+const User = require("./models/User");
+
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(cors(corsOpts))
+
+const mongoDB = process.env.MONGO_DB;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+// Set up local strategy for our authentication (passport.authentication() uses these settings)
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      // Error occurred in our search
+      if (err) return done(err);
+
+      // Validates username
+      if (!user) {
+        return done(null, false);
+      }
+
+      // Validates password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // Password authenticated
+          return done(null, user);
+        } else {
+          // passwords do not match
+          return done(null, false);
+        }
+      });
+    });
+  })
+);
+
+// Define Routes
+app.use("/", userRoutes);
+
+module.exports = app;
